15ba0c0c8b92377077b5a9c2d5ffebc927928a62
Pelayo <phuermij@myuax.com>
2022-11-19 19:33:30 +0100

    arbol huffman

--- Arbol_Huffman/Huffman.py
+++ Arbol_Huffman/Huffman.py
@@ -1,7 +1,60 @@
 class NodoArbol:
-    def __init__(self, info, prob):
+    def __init__(self, info, prob, izq=None, der=None):
         self.info = info
         self.prob = prob
-        self.izq = None
-        self.der = None
+        self.izq = izq
+        self.der = der
+
+
+class ListaNodos():
+    def __init__(self):
+        self.lista = []
+    
+    def agregar_nodo(self, nodo):
+        self.lista.append(nodo)
+
+    def sort(self):
+        i = 0
+        control = True
+        while i <= len(self.lista)-2 and control:
+            control = False
+            for j in range(0, len(self.lista)-i-1):
+                if self.lista[j].prob > self.lista[j+1].prob:
+                    self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
+                    control = True
+        i += 1
+        return self.lista
+    
+    def barrido(self):
+        i = 0
+        while i < len(self.lista):
+            print(self.lista[i].der.izq.izq.prob)
+            i += 1
+        else:
+            return
+    def eliminar_nodo(self, clave):
+        self.lista.remove(clave)
+
+    def suma(self):
+        control = True
+        while control:
+            control = False
+            if len(self.lista) > 1:
+                prob = self.lista[0].prob + self.lista[1].prob
+                nuevo_nodo = NodoArbol(None, prob, self.lista[0], self.lista[1])
+                self.agregar_nodo(nuevo_nodo)
+                self.eliminar_nodo(self.lista[0])
+                self.eliminar_nodo(self.lista[0])
+                self.sort()
+                control = True
+                
+
+
+    
         
+
+
+                
+    
+
+
---
+++ Arbol_Huffman/mainej1.py
@@ -0,0 +1,25 @@
+from Huffman import *
+
+nodo1 = NodoArbol("A", 0.2)
+nodo2 = NodoArbol("F", 0.17)
+nodo3 = NodoArbol("1", 0.13)
+nodo4 = NodoArbol("3", 0.21)
+nodo5 = NodoArbol("0", 0.05)
+nodo6 = NodoArbol("M", 0.09)
+nodo7 = NodoArbol("T", 0.15)
+
+lista = ListaNodos()
+
+lista.agregar_nodo(nodo1)
+lista.agregar_nodo(nodo2)
+lista.agregar_nodo(nodo3)
+lista.agregar_nodo(nodo4)
+lista.agregar_nodo(nodo5)
+lista.agregar_nodo(nodo6)
+lista.agregar_nodo(nodo7)
+
+lista.sort()
+
+lista.suma()
+
+lista.barrido()
