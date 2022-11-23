5b3c505c8e0f9ceb27256c82505519fd2ef4c82f
Pelayo <phuermij@myuax.com>
2022-11-23 22:38:26 +0100

    insertar vertice

--- Grafo_maravillas/Maravillas.py
+++ Grafo_maravillas/Maravillas.py
@@ -11,14 +11,30 @@ class NodoVertice():
         self.visitado = False 
         self.adyacentes = Arista()
 
+
+class Arista():
+    def __init__(self):
+        self.inicio = None
+        self.tamanio = 0     
+        
 class Grafo():
     def __init__(self, dirigido= False):
         self.inicio = None
         self.dirigido = dirigido
         self.tamanio = 0
 
-class Arista():
-    def __init__(self):
-        self.inicio = None
-        self.tamanio = 0      
+    def insertar_vertice(self, dato):
+        nodo = NodoVertice(dato)
+        if self.inicio is None or self.inicio.info > dato:
+            nodo.sig = self.inicio
+            self.inicio = nodo
+        else:
+            ant = self.inicio
+            act =  self.inicio.info
+            while act is not None and act.info < nodo.info:
+                ant = act
+                act = act.sig
+            nodo.sig = act
+            ant.sig = nodo
+        self.tamanio += 1
     
