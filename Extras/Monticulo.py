11cdf823c4d6d0279b8707cbeafe9111afb8663c
Pelayo <phuermij@myuax.com>
2022-11-24 11:09:43 +0100

    intercambiar

--- Extras/Monticulo.py
+++ Extras/Monticulo.py
@@ -7,7 +7,13 @@ class Monticulo():
         while indice > 0 and self.vector[indice] > self.vector[(indice - 1)//2]:
             padre = (indice - 1) // 2
             self.intercambio(self.vector, indice, padre)
+            indice = padre
     
     def agregar(self, dato):
         self.vector[self.tamanio] - dato
         self.flotar(self.tamanio)
+        self.tamanio += 1
+    
+    def intercambio(self, indice, padre):
+        self.vector[indice], self.vector[padre] = self.vector[padre], self.vector[indice]
+
