60105dd5f57ee2c938c7d81381f1621c8c7bd1e8
Pelayo <phuermij@myuax.com>
2022-11-24 11:11:44 +0100

    quitar

--- Extras/Monticulo.py
+++ Extras/Monticulo.py
@@ -13,6 +13,13 @@ class Monticulo():
         self.vector[self.tamanio] - dato
         self.flotar(self.tamanio)
         self.tamanio += 1
+
+    def quitar(self):
+        self.intercambio(0, self.tamanio-1)
+        dato = self.vector[self.tamanio-1]
+        self.tamanio -= 1
+        self.hundir(0)
+        return dato
     
     def intercambio(self, indice, padre):
         self.vector[indice], self.vector[padre] = self.vector[padre], self.vector[indice]
