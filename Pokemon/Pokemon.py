a427ae8acc475e1d4f12a8d63991772d0c65b24c
Pelayo <phuermij@myuax.com>
2022-11-24 20:28:30 +0100

    cambios

--- Pokemon/Pokemon.py
+++ Pokemon/Pokemon.py
@@ -5,3 +5,30 @@ def filtrar(csv):
     pokemon.rename(columns={"#" : "id"}, inplace=True)
     pokemon = pokemon[["id", "Name", "Type 1"]]
     return pokemon
+
+def agregar(csv, etiqueta, arbol):
+    raiz = arbol.raiz.info
+    for i in csv[etiqueta]:
+        arbol.insertar_nodo(raiz, i)
+    return arbol
+
+def buscar(arbol, buscado):
+    encontrados = []
+    aux = arbol.raiz
+    while aux is not None:
+        if buscado in aux.info:
+            encontrados.append(aux.info)
+        aux = aux.sig
+    return encontrados
+
+def buscar_tipos(arbol, tipo):
+    pos = []
+    i = 0
+    aux = arbol.raiz
+    while aux is not None:
+        if tipo == aux.info:
+            pos.append(i)
+        aux = aux.sig
+        i += 1
+    return pos
+
--- Pokemon/mainej2.py
+++ Pokemon/mainej2.py
@@ -1,11 +1,23 @@
 from Arbol import *
-from pokemon import filtrar
+from pokemon import filtrar, agregar, buscar, buscar_tipos
 
 if __name__ == "__main__":
     pokemon = filtrar("Pokemon/pokemon.csv")
 
+    #Parte 1
     arbol_nombres = Arbol()
+    agregar(arbol_nombres, "Pokemon/pokemon.csv", "Name")
     arbol_tipos = Arbol()
+    agregar(arbol_tipos, "Pokemon/pokemon.csv", "Type 1")
     arbol_id = Arbol()
+    agregar(arbol_id, "Pokemon/pokemon.csv", "id")
 
+    #Parte 2
+    buscar(arbol_nombres, "Lo que quieres buscar")
+
+    #Parte 3
+    pos = buscar_tipos(arbol_tipos, "El tipo que quieras buscar")
+    for i in pos:
+        print(pokemon["Name"][i])
     
+    #Parte 4
