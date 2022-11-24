b5b12660cdb6d974658edaddedba0034e0196b3c
Pelayo <phuermij@myuax.com>
2022-11-24 11:15:55 +0100

    hundir

--- Extras/Monticulo.py
+++ Extras/Monticulo.py
@@ -9,6 +9,22 @@ class Monticulo():
             self.intercambio(self.vector, indice, padre)
             indice = padre
     
+    def hundir(self, indice):
+        hijo_izq = (indice * 2) + 1
+        control = True
+        while control and hijo_izq < self.tamanio:
+            hijo_der = hijo_izq + 1
+            aux = hijo_izq
+            if hijo_der < self.tamanio:
+                if self.vector[hijo_der] > self.vector[hijo_izq]:
+                    aux = hijo_der
+            elif self.vector[indice] < self.vector[aux]:
+                self.intercambio(indice, aux)
+                indice = aux
+                hijo_izq = (indice * 2) + 1
+            else:
+                control = False
+    
     def agregar(self, dato):
         self.vector[self.tamanio] - dato
         self.flotar(self.tamanio)
