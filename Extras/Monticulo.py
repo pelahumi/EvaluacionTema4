f386892e4d82eaae276dfbeecd8cce78e595d2b7
Pelayo <phuermij@myuax.com>
2022-11-24 10:59:24 +0100

    funcion flotar

--- Extras/Monticulo.py
+++ Extras/Monticulo.py
@@ -3,4 +3,11 @@ class Monticulo():
         self.tamanio = 0
         self.vector = [None] * tamanio
     
-    def agregar
+    def flotar(self, indice):
+        while indice > 0 and self.vector[indice] > self.vector[(indice - 1)//2]:
+            padre = (indice - 1) // 2
+            self.intercambio(self.vector, indice, padre)
+    
+    def agregar(self, dato):
+        self.vector[self.tamanio] - dato
+        self.flotar(self.tamanio)
